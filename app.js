5aba61feea536cfc23ada63258fd3a7f7ac4ff20
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-08-24 10:40:34 +0200

    Del 5 og 6 virker vist også.

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 const port = 3333;
 
-app.listen(port, () => {console.log(`App running on http://localhost:${port}`); });
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
 
-app.get("/", (request, response) => { response.send("Hello world"); });
+app.get("/", (request, response) => {
+  response.send("Hello world");
+});
 
 app.get("/test", (request, response) => {
   response.send("This is a test route");
+});
+
+// app.get("/users", (request, response) => {
+//   response.send("My users endpoint!");
+// });
+
+app.get("/users", async (request, response) => {
+    const data = await fs.readFile("data.json");
+    const users = JSON.parse(data);
+    console.log(users);
+    response.json(users);
 });
